f7189adf3f4e2fc6840b09f883ceeaa14cfc8049
kuroazai <pyromwansa@gmail.com>
2019-10-31 09:08:23 +0000

    Update Fib.py

--- Algorithms/Fib.py
+++ Algorithms/Fib.py
@@ -5,34 +5,19 @@ Created on Thu Mar 21 15:59:02 2019
 @author: KuroAzai
 """
 
-import time
-
-def fibsequence(element):   
-    #Creating our array with our base values for our fibonacci sequence
-    fib = [0 , 1]
-    #A counter that will keep track of the iterations that have passed 
-    counter = 0 
+def fibsequence(element):
     
-    #A while loop that will iterate until our desired element has been discoved
-    while element != counter: 
-        #We count the size of our fib array for our algorithm to work
-        size = len(fib) - 1
-        #Each iteration we add the the last and second last elements together to give us our next fib sequence
-        s = fib[size -1] + fib[size]
-        print("\nn = ", counter, fib[-2:], s)
-        #We append the result to our fib array
-        fib.append(s)
-        #Increase the counter by 1 to keep record of the iterations passed
-        counter += 1
-        #misc : How long we want our system to wait before each calculation
-        time.sleep(0.25)
+    if element < 0:
+       return print("Invalid Entry")
+        
+    elif element ==1:
+        return 0
+    elif element==2:
+        return 1
+    else:
+        return fibsequence(element-1) + fibsequence(element-2)
     
-    #We return the element we wished to get from our fib array 
-    return fib[element]
-
-
-#We call the print function on the fibsequence, that will return the result of our return statement. 
-print(fibsequence(10))
+print(fibsequence(9))
 
 
 
